6d94646fa091659d4baab28e835126177fe9a42c
bishal2408 <bishallamichhane24@gmail.com>
2022-07-07 08:38:50 +0545

    First django push

---
+++ Real Estate/RealEstate/agent/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Agent(models.Model):
+    title = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    experiencesInYears = models.IntegerField(default=0)
+    rating = models.CharField(max_length=255)
+    area = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='property/%Y/%m/%d', null=True)
+    created = models.DateField(auto_now=True)
+    updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ Real Estate/RealEstate/query/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from property.models import Property
+
+# Create your models here.
+
+class Query(models.Model):
+    title = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, null=True)
+    message = models.TextField()
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
